8f2a5ff1953595c3467dc1c5a55ba3a446de3a89
Adriano Raiano <adriano@raiano.ch>
2022-12-21 12:09:20 +0100

    extend tests

--- i18nextChainedBackend.js
+++ i18nextChainedBackend.js
@@ -10,13 +10,39 @@
     }
   }
 
+  function _typeof(obj) {
+    "@babel/helpers - typeof";
+
+    return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) {
+      return typeof obj;
+    } : function (obj) {
+      return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
+    }, _typeof(obj);
+  }
+
+  function _toPrimitive(input, hint) {
+    if (_typeof(input) !== "object" || input === null) return input;
+    var prim = input[Symbol.toPrimitive];
+    if (prim !== undefined) {
+      var res = prim.call(input, hint || "default");
+      if (_typeof(res) !== "object") return res;
+      throw new TypeError("@@toPrimitive must return a primitive value.");
+    }
+    return (hint === "string" ? String : Number)(input);
+  }
+
+  function _toPropertyKey(arg) {
+    var key = _toPrimitive(arg, "string");
+    return _typeof(key) === "symbol" ? key : String(key);
+  }
+
   function _defineProperties(target, props) {
     for (var i = 0; i < props.length; i++) {
       var descriptor = props[i];
       descriptor.enumerable = descriptor.enumerable || false;
       descriptor.configurable = true;
       if ("value" in descriptor) descriptor.writable = true;
-      Object.defineProperty(target, descriptor.key, descriptor);
+      Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor);
     }
   }
   function _createClass(Constructor, protoProps, staticProps) {
--- test/typescript/basic.test-d.ts
+++ test/typescript/basic.test-d.ts
@@ -1,9 +1,18 @@
 import i18next from 'i18next';
-import Backend from 'i18next-chained-backend';
+import Backend, { ChainedBackendOptions } from 'i18next-chained-backend';
+import LocizeBackend from 'i18next-locize-backend';
+import HTTPBackendLocal from 'i18next-http-backend';
 
-i18next.use(Backend).init({
+i18next.use(Backend).init<ChainedBackendOptions>({
   backend: {
-    backends: [],
-    backendOptions: []
+    backends: [
+      LocizeBackend,  // primary
+      HTTPBackendLocal  // fallback
+    ],
+    backendOptions: [{
+      projectId: 'myLocizeProjectId'
+    }, {
+      loadPath: '/locales/{{lng}}/{{ns}}.json' // http api load path for my own fallback
+    }]
   },
 });
